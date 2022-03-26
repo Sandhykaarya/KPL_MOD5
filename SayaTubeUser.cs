40bf2f863d7583a9ca92b93b0f1734f1cfafb99d
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-03-26 17:14:40 +0700

    penambahan design by contract pada semua kelas

--- SayaTubeUser.cs
+++ SayaTubeUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
 
 namespace modul5_1302204002
 {
@@ -18,16 +19,21 @@ namespace modul5_1302204002
             id = random.Next(10000, 99999);
             uploadedVideos = new List<SayaTubeVideo>();
             this.username = x;
+
+            Contract.Requires(username.Length <= 100);
+            Contract.Requires(username != null);
         }
 
         public int getTotalVideoPlayCount()
         {
+            Contract.Requires(uploadedVideos.Count <= 2147483647);
             return uploadedVideos.Count;
         }
 
         public void addVideo(SayaTubeVideo x)
         {
             uploadedVideos.Add(x);
+            Contract.Requires(uploadedVideos != null);
         }
 
         public void printAllVideoPlayCount()
@@ -37,6 +43,8 @@ namespace modul5_1302204002
             {
                 Console.WriteLine("Video " + (i+1) + " judul: " + uploadedVideos[i].getTitle());
             }
+
+            Contract.Ensures(uploadedVideos.Count < 9);
         }
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
 
 namespace modul5_1302204002
 {
@@ -10,19 +11,36 @@ namespace modul5_1302204002
     {
         private int id;
         private string title;
-        private int playCount;
-        Random random = new Random(); 
+        private int playCount = 2147483641;
+        Random random = new Random();
 
         public SayaTubeVideo(string x)
-        {
+        { 
             id = random.Next(10000,99999);
             playCount = 0;
             this.title = x;
+
+            Contract.Requires(title.Length <= 200);
+            Contract.Requires(title != null);
         }
 
         public void increasePlayCount(int x)
         {
             playCount+=1;
+            Contract.Requires(playCount <= 25000000);
+            Contract.Requires(playCount > 0);
+
+            checked
+            {
+                try
+                {
+                    playCount = 2147483647;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Play Count melebihi batas tertinggi integer");
+                }
+            }
         }
 
         public void PrintVideoDetails()
